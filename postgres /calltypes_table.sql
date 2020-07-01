3e7a7a928a98d9e2c9bc822a107e38fe274de073
Rajnish Rao <rajnishprao@gmail.com>
2020-07-01 21:23:11 +0200

    update

---
+++ postgres /calltypes_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE CallTypes (
+calltype_id INT PRIMARY KEY,
+abbr VARCHAR(20),
+calltype VARCHAR(50),
+description VARCHAR(50),
+modulation_type VARCHAR(10),
+category VARCHAR(50),
+color VARCHAR(10)
+);
+
