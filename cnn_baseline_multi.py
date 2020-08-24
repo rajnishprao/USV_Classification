4e7a3552fe9d86ef42a5f34355f26e635c783ddd
Rajnish Rao <rajnishprao@gmail.com>
2020-08-24 12:08:16 +0200

    final version

--- cnn_baseline_multi.py
+++ cnn_baseline_multi.py
@@ -1,145 +1,163 @@
-'''
+"""
 Automated Classification of Rat Ultrasonic Vocalizations (USVs):
 multi-class categorisation of flat, complex and trill calltypes
-'''
+"""
 
 
 import os
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.metrics import classification_report, confusion_matrix
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten, Activation
-from keras.layers import Conv2D, MaxPool2D, BatchNormalization
-from keras.preprocessing.image import ImageDataGenerator
-from keras import regularizers
-from keras.callbacks import EarlyStopping
-from keras import backend as K
 
 # Activate PlaidML backend
 os.environ["KERAS_BACKEND"] = "plaidml.keras.backend"
 
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from keras import backend as K
+from keras import regularizers
+from keras.callbacks import EarlyStopping
+from keras.layers import (Activation, BatchNormalization, Conv2D, Dense,
+                          Dropout, Flatten, MaxPool2D)
+from keras.models import Sequential
+from keras.preprocessing.image import ImageDataGenerator
+from sklearn.metrics import classification_report, confusion_matrix
+
 # Data Generators
-train_datagen = ImageDataGenerator(rescale=1./255)
-validation_datagen = ImageDataGenerator(rescale=1./255)
-test_datagen = ImageDataGenerator(rescale=1./255)
+train_datagen = ImageDataGenerator(rescale=1.0 / 255)
+validation_datagen = ImageDataGenerator(rescale=1.0 / 255)
+test_datagen = ImageDataGenerator(rescale=1.0 / 255)
 
 train_ds = train_datagen.flow_from_directory(
-    './training_data',
+    "./training_data",
     target_size=(334, 217),
-    color_mode='rgb',
+    color_mode="rgb",
     batch_size=32,
-    class_mode='categorical')
+    class_mode="categorical",
+)
 
 validation_ds = validation_datagen.flow_from_directory(
-    './validation_data',
+    "./validation_data",
     target_size=(334, 217),
-    color_mode='rgb',
+    color_mode="rgb",
     batch_size=32,
-    class_mode='categorical')
+    class_mode="categorical",
+)
 
 test_ds = test_datagen.flow_from_directory(
-    './test_data',
+    "./test_data",
     target_size=(334, 217),
-    color_mode='rgb',
+    color_mode="rgb",
     batch_size=1,
-    class_mode='categorical',
-    shuffle=False)
+    class_mode="categorical",
+    shuffle=False,
+)
 
 # Callbacks
 K.clear_session()
-callback = EarlyStopping(monitor='val_loss', mode='min', patience=10)
+callback = EarlyStopping(monitor="val_loss", mode="min", patience=10)
 
 # Convolutional Neuronal Network
 # based on CNN from Arabic/Kannada MNIST transfer learning project
-model = Sequential([
+model = Sequential(
+    [
+        Conv2D(
+            filters=8,
+            kernel_size=(3, 3),
+            kernel_initializer="he_normal",
+            padding="same",
+            input_shape=(334, 217, 3),
+            kernel_regularizer=regularizers.l2(0.01),
+        ),
+        MaxPool2D(pool_size=(2, 2)),
+        BatchNormalization(),
+        Activation("relu"),
+        Dropout(0.2),
+        Conv2D(
+            filters=16,
+            kernel_size=(3, 3),
+            padding="same",
+            kernel_regularizer=regularizers.l2(0.01),
+        ),
+        MaxPool2D(pool_size=(2, 2)),
+        BatchNormalization(),
+        Activation("relu"),
+        Dropout(0.2),
+        Conv2D(
+            filters=32,
+            kernel_size=(3, 3),
+            padding="same",
+            kernel_regularizer=regularizers.l2(0.01),
+        ),
+        MaxPool2D(pool_size=(2, 2)),
+        BatchNormalization(),
+        Activation("relu"),
+        Dropout(0.2),
+        Flatten(),
+        Dense(64, kernel_regularizer=regularizers.l2(0.01)),
+        Dense(3, kernel_regularizer=regularizers.l2(0.01)),
+        BatchNormalization(),
+        Activation("softmax"),
+    ]
+)
 
-    Conv2D(filters=8, kernel_size=(3, 3), kernel_initializer='he_normal',
-           padding='same', input_shape=(334, 217, 3),
-           kernel_regularizer=regularizers.l2(0.01)),
-    MaxPool2D(pool_size=(2, 2)),
-    BatchNormalization(),
-    Activation('relu'),
-    Dropout(0.2),
+model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
 
-    Conv2D(filters=16, kernel_size=(3, 3), padding='same',
-           kernel_regularizer=regularizers.l2(0.01)),
-    MaxPool2D(pool_size=(2, 2)),
-    BatchNormalization(),
-    Activation('relu'),
-    Dropout(0.2),
-
-    Conv2D(filters=32, kernel_size=(3, 3), padding='same',
-           kernel_regularizer=regularizers.l2(0.01)),
-    MaxPool2D(pool_size=(2, 2)),
-    BatchNormalization(),
-    Activation('relu'),
-    Dropout(0.2),
-
-    Flatten(),
-    Dense(64, kernel_regularizer=regularizers.l2(0.01)),
-    Dense(3, kernel_regularizer=regularizers.l2(0.01)),
-    BatchNormalization(),
-    Activation('softmax')
-])
-
-model.compile(optimizer='adam', loss='categorical_crossentropy',
-              metrics=['accuracy'])
-
-history = model.fit_generator(train_ds, epochs=50, steps_per_epoch=32,
-                              validation_data=validation_ds,
-                              validation_steps=32, callbacks=[callback],
-                              class_weight={0:1.0, 1:1.4, 2:1.1})
+history = model.fit_generator(
+    train_ds,
+    epochs=50,
+    steps_per_epoch=32,
+    validation_data=validation_ds,
+    validation_steps=32,
+    callbacks=[callback],
+    class_weight={0: 1.0, 1: 1.4, 2: 1.1},
+)
 
 # Plot loss
-loss_train = history.history['loss']
-val_loss = history.history['val_loss']
+loss_train = history.history["loss"]
+val_loss = history.history["val_loss"]
 epochs = range(0, len(loss_train))
-plt.plot(epochs, loss_train, 'g', label='Training Loss')
-plt.plot(epochs, val_loss, 'k', label='Validation Loss')
-plt.title('Loss: Training and Validation')
-plt.xlabel('Epochs')
+plt.plot(epochs, loss_train, "g", label="Training Loss")
+plt.plot(epochs, val_loss, "k", label="Validation Loss")
+plt.title("Loss: Training and Validation")
+plt.xlabel("Epochs")
 plt.legend()
-plt.savefig('loss.png', dpi=300)
+plt.savefig("loss.png", dpi=300)
 plt.show()
 
 # # Plot accuracy
-acc_train = history.history['acc']
-val_acc = history.history['val_acc']
+acc_train = history.history["acc"]
+val_acc = history.history["val_acc"]
 epochs = range(0, len(acc_train))
-plt.plot(epochs, acc_train, 'b', label='Training Accuracy')
-plt.plot(epochs, val_acc, 'r', label='Validation Accuracy')
-plt.title('Accuracy: Training and Validation')
-plt.xlabel('Epochs')
+plt.plot(epochs, acc_train, "b", label="Training Accuracy")
+plt.plot(epochs, val_acc, "r", label="Validation Accuracy")
+plt.title("Accuracy: Training and Validation")
+plt.xlabel("Epochs")
 plt.ylim(0, 1)
 plt.legend()
-plt.savefig('accuracy.png', dpi=300)
+plt.savefig("accuracy.png", dpi=300)
 plt.show()
 
 # Calculate test scores
 test_loss, test_acc = model.evaluate_generator(test_ds, steps=50)
-print('Test Loss:', test_loss)
-print('Test Accuracy:', test_acc)
+print("Test Loss:", test_loss)
+print("Test Accuracy:", test_acc)
 
 # Confusion Matrix
 Y_pred = model.predict_generator(test_ds)
 y_pred = np.argmax(Y_pred, axis=1)
 cm = confusion_matrix(test_ds.classes, y_pred)
-print('Confusion Matrix - Test Data Set')
+print("Confusion Matrix - Test Data Set")
 print(cm)
-index = ['complex', 'flat', 'trill']
-columns = ['complex', 'flat', 'trill']
+index = ["complex", "flat", "trill"]
+columns = ["complex", "flat", "trill"]
 cm_df = pd.DataFrame(cm, columns, index)
-sns.heatmap(cm_df/np.sum(cm_df), annot=True, fmt='.2%',
-            cmap='Blues', cbar=False)
-plt.title('Confusion Matrix')
-plt.xlabel('True Classes')
-plt.ylabel('Predicted Classes')
-plt.savefig('confusion_matrix.png', dpi=300)
+sns.heatmap(cm_df / np.sum(cm_df), annot=True, fmt=".2%", cmap="Blues", cbar=False)
+plt.title("Confusion Matrix")
+plt.xlabel("True Classes")
+plt.ylabel("Predicted Classes")
+plt.savefig("confusion_matrix.png", dpi=300)
 plt.show()
 
 # Classification Report
-print('Classification Report - Test Data Set')
+print("Classification Report - Test Data Set")
 print(classification_report(test_ds.classes, y_pred, target_names=columns))
